32f996099c5a13a94d5b89f4b292e0413b4040a6
Juan Balut <jbalutr@gmail.com>
2020-05-23 14:17:48 -0400

    more typo, blog data base

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,6 +6,7 @@ class Project(models.Model):
 	image = models.ImageField(upload_to='portfolio/images/')
 	url = models.URLField(blank=True)
 	blog_entry = models.URLField(blank=True)
+
 		
 	def __str__(self):
 		return self.title
